20e878bc74976b50f93ca0321bcc48ff30e6307b
nano <df@proconsis.ch>
2025-07-10 21:31:30 +0200

    Added Backend

---
+++ backend/src/test/java/anonnanoo/dcs/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package anonnanoo.dcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
